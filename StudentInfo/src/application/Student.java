047e2d6b456ed4a6972a8c9e3fbdb2468a3ca462
Ravi <raviramchand8@gmail.com>
2021-08-11 14:29:23 -0400

    subclass of person class

---
+++ StudentInfo/src/application/Student.java
@@ -0,0 +1,26 @@
+package application;
+
+public class Student extends Person{
+	//Field variables
+	private double _gpa;	
+	
+	//Constructors 
+	public Student(String newName, String newAddress, double gpa) {
+		super(newName, newAddress);
+		this._gpa = gpa;
+	}	
+	
+	//Return toString method from Person class with gpa added
+	@Override
+	public String toString() {
+		return super.toString() +" " +this._gpa;
+	}
+	
+	//Implement the abstract method in the Person class
+	public String currentStatus() {
+		return "Student";
+	}
+	
+	
+	//End of Student class
+}
