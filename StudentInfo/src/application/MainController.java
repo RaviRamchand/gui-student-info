4c02345d05ab64ddc5b6b6a0ecb7dd0f4a609df2
Ravi <raviramchand8@gmail.com>
2021-08-11 15:15:10 -0400

    user inputs now stored into an array list

--- StudentInfo/src/application/MainController.java
+++ StudentInfo/src/application/MainController.java
@@ -1,6 +1,8 @@
 package application;
 
 
+import java.util.ArrayList;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -11,13 +13,21 @@ public class MainController {
 	//Controllers 	
 	@FXML private TextField _txtName;
 	@FXML private TextField _txtAdd; 
-	@FXML private TextField _textGpa;
+	@FXML private TextField _txtGpa;
 	
 	@FXML private Button _btnAdd;
 	@FXML private Button _btnSort;
 	@FXML private Button _btnDis;
 	
+	//Fields 
+	String name;
+	String add;
+	double gpa;
 	
+	//ArrayList of type Student to student objects in 
+	ArrayList<Student> stArr = new ArrayList<Student>();
+	
+	//Handlers
 	@FXML private void initialize() {
 		_btnAdd.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent e) {
@@ -38,8 +48,24 @@ public class MainController {
 		});
 	}
 	
+	//Methods
 	private void addHandle() {
-		System.out.println("Add Button");
+		//TODO add text into the boxes.
+		
+		//Convert user input into a string
+		name = _txtName.getText();
+		add = _txtAdd.getText(); 
+		
+		//Convert user input into a double
+		gpa = Double.parseDouble(_txtGpa.getText());	
+		
+		//Create a Student class object to initalise the field variables in Person class
+		Student stObj = new Student(name, add, gpa);
+		
+		//Add the values in the student object into the array list
+		stArr.add(stObj);
+		
+		System.out.println(stArr.toString());
 	}
 	
 	private void sortHandle() {
