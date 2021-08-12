b9ebd049d243b954ba85f69a5132abfc6fc74298
Ravi <raviramchand8@gmail.com>
2021-08-12 12:11:51 -0400

    print sorted arraylist into new gui window

--- StudentInfo/src/application/SecondController.java
+++ StudentInfo/src/application/SecondController.java
@@ -1,7 +1,23 @@
 package application;
 
+import java.util.ArrayList;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 
 public class SecondController {
-
+	@FXML private TextArea _txtBox;
+	
+	
+	@FXML private void initialize() {
+		
+		String display =  "";
+		ArrayList<Student> st = MainController.stArr;
+		
+		for(int i = 0; i<st.size(); i++) {
+			display = st.toString();
+		}
+		
+		_txtBox.setText(display);
+	}
 }
